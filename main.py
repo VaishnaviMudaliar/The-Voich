6434df66314614010a779927e909463cdc93b381
VaishnaviMudaliar <vaishnavim1311@gmail.com>
2023-07-30 11:13:23 +0000

    Updated comments

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import streamlit as st
-# import pandas as pd
-# from io import StringIO
-# import base64
 from PyPDF2 import PdfReader
-# from elevenlabs import set_api_key
-# import os
-# import requests
-# from elevenlabs import generate, play
-
 from elevenlabs import generate, set_api_key
 st.title("The Voich")
+
+# funtion to generate text to speech
 def generate_voice(text):
   set_api_key('5bde68c411f0c6779da15eb8d76ca7b1')
   audio = generate(
@@ -18,14 +12,18 @@ def generate_voice(text):
          model='eleven_multilingual_v1'
     )
   st.audio(audio)
-
+# uploading of file
 uploaded_file = st.file_uploader("Choose a file" , type = 'pdf')
+# if the file object is not none , then extract text from it
 if uploaded_file is not None:
   reader = PdfReader(uploaded_file)
   page = reader.pages[5]
   text = page.extract_text()
   st.write(text)
+  # pass the text to elevenlabs to show its magic
   st.button('Read' , on_click = generate_voice(text))
+
+
 
 
 
